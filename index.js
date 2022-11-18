1ac000949d330b532de2a3a9fd3cdcfb35d6b31b
Andrey <sherhnev.andrey2003@gmail.com>
2022-11-18 15:45:00 +0300

    Подключение к базе данных MongoDB

--- index.js
+++ index.js
@@ -4,15 +4,26 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const path = require('path');
+const config = require("./config/db");
 
 const app = express();
 
 const port = 3000;
 
+mongoose.connect(config.db, {useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connection.on("connected", () => {
+    console.log('Succesfull connection to the database')
+});
+
+mongoose.connection.on("error", (err) => {
+    console.log('Not Succesfull connection to the database' + err)
+});
+
 app.listen(port, () => {
-    console.log("Сервер был запущен на порту: " + port)
+    console.log("The server was runnung on the port: " + port)
 });
 
 app.get("/", (req, res) => {
-    res.send("Главная страница блога1")
+    res.send("Home page")
 });
