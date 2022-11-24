dde6d76f4864187b0defdee0618b86794748dab7
ShERAand <sherhnev.andrey2003@gmail.com>
2022-11-24 22:30:32 +0300

    Подключение базы удаленно,Доработки по стилям

--- App/config/db.js
+++ App/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
     // db: "mongodb://localhost:27017/blog",
-    db: "mongodb+srv://ggenius:1qaz2wsx@cluster0.2wq5hkn.mongodb.net/?retryWrites=true&w=majority",
+    db: "mongodb+srv://Admin:Admin@atlascluster.f6be9xv.mongodb.net/Blog?retryWrites=true&w=majority",
     secret: "some_secret_key"
 }
